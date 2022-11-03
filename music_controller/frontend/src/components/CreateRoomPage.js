48baeb2f130709abbff5da0265bdb82d7bc5ab9d
Bishal Khanal <bishal.khanal87@gmail.com>
2022-11-03 16:18:58 -0700

    send POST request from front to backend

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -16,11 +16,45 @@ export default class CreateRoomPage extends Component{
 
   constructor(props){
     super(props);
+    this.state = {
+      guestCanPause: true,
+      votesToSkip:  this.defaultVotes,
+    };
+    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
 
   }
 
+  handleVotesChange(e) {
+    this.setState({
+      votesToSkip: e.target.value,
+    });
+  }
+
+  handleGuestCanPauseChange(e) {
+    this.setState({
+      guestCanPause: e.target.value == 'true' ? true : false,
+    });
+  }
+
+  handleRoomButtonPressed(){
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        votes_to_skip: this.state.votesToSkip,
+        guest_can_pause: this.state.guestCanPause
+      }),
+    };
+    fetch('/api/create-room', requestOptions).then((response) =>
+    response.json()
+    ).then((data) => console.log(data));
+  }
+
   render(){
-    return <Grid container spacing={1}>
+    return (
+    <Grid container spacing={1}>
       <Grid item xs={12} align="center">
         <Typography component='h4' variant='h4'>
           Create A Room
@@ -30,22 +64,28 @@ export default class CreateRoomPage extends Component{
       <Grid item xs={12} align="center">
         <FormControl component="fieldset">
           <FormHelperText>
-            <div align='center'>
+            <div align="center">
               Guest Control of Playback State
             </div>
           </FormHelperText>
-          <RadioGroup row defaultValue='true'>
-            <FormControlLabel value="true" 
-              control={<Radio color='primary' />}>
+          <RadioGroup 
+            row 
+            defaultValue="true"
+            onChange={this.handleGuestCanPauseChange}>
+            <FormControlLabel 
+              value="true" 
+              control={<Radio color='primary' />}
               label="Play/Pause"
               labelPlacemant="bottom"
-            </FormControlLabel>
+              />
+            
 
             <FormControlLabel value="false" 
-              control={<Radio color='secondary' />}>
+              control={<Radio color='secondary' />}
               label="No Control"
               labelPlacemant="bottom"
-            </FormControlLabel>
+              />
+            
             
           </RadioGroup>
         </FormControl>
@@ -54,7 +94,9 @@ export default class CreateRoomPage extends Component{
             <FormControl>
              <TextField 
              required={true} 
-             type='number'
+             
+             type="number"
+             onChange={this.handleVotesChange}
               defaultValue={this.defaultVotes}
               imputProps={{
                 min: 1,
@@ -68,19 +110,27 @@ export default class CreateRoomPage extends Component{
                   </div>
                 </FormHelperText>
             </FormControl>
-            <Grid item xs={12} align='center'>
-              <Button color="primary" variant="contained">
+            </Grid>
+
+            <Grid item xs={12} align="center">
+              <Button 
+              color="primary" 
+              variant="contained"
+              onClick={this.handleRoomButtonPressed}>
                 Create A Room
               </Button>
             </Grid>
 
-            <Grid item xs={12} align='center'>
-              <Button color="secondary" variant="contained" to="/" component={Link}>
+            <Grid item xs={12} align="center">
+              <Button 
+              color="secondary" 
+              variant="contained" 
+              to="/" component={Link}>
                 Back
               </Button>
             </Grid>
 
           </Grid>
-    </Grid>
+    )
   }
 }
