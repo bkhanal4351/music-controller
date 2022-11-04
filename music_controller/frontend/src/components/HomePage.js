699aabafba23d6e90229b30029caabd482e0b748
Bishal Khanal <bishal.khanal87@gmail.com>
2022-11-03 19:32:31 -0700

    set up fetch and request

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import { BrowserRouter as Router, Switch, Route, Link, redirect } from "react-router-dom"
+import Room from "./Room";
 
 export default class HomePage extends Component{
   constructor(props){
@@ -15,6 +16,7 @@ export default class HomePage extends Component{
         <Route exact path='/'><p>This is the Home page</p></Route>
         <Route path='/join' component={RoomJoinPage}></Route>
         <Route path='/create' component={CreateRoomPage}></Route>
+        <Route path='/room/:roomCode' component={Room} />
       </Switch>
 
     </Router>
