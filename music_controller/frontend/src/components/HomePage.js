8d9eed55538f508e6af85ed5528595f4a76d8a73
Bishal Khanal <bishal.khanal87@gmail.com>
2022-11-04 21:01:51 -0700

    adds callback to clear room code and fix backend bug

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,28 +1,33 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter as Router, Switch, Route, Link, redirect, Redirect } from "react-router-dom"
 import Room from "./Room";
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-
-export default class HomePage extends Component{
-  constructor(props){
+export default class HomePage extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       roomCode: null,
     };
-
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
     fetch("/api/user-in-room")
-    .then((response) => response.json())
-    .then((data) => {
-      this.setState({
-        roomCode: data.code,
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
       });
-    });
   }
 
   renderHomePage() {
@@ -47,6 +52,12 @@ export default class HomePage extends Component{
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
@@ -64,7 +75,12 @@ export default class HomePage extends Component{
           />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
