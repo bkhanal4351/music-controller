a3d48e64a1f345de138a74fcd6428bfbaabc3241
Bishal Khanal <bishal.khanal87@gmail.com>
2022-11-07 23:27:53 -0800

    add play/pause feature

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -33,7 +33,7 @@ export default class CreateRoomPage extends Component {
     this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
     this.handleVotesChange = this.handleVotesChange.bind(this);
     this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
-    this.handleUpdateButtonPressed = this.handleUpdateButtonPressed.bind(this)
+    this.handleUpdateButtonPressed = this.handleUpdateButtonPressed.bind(this);
   }
 
   handleVotesChange(e) {
@@ -72,81 +72,85 @@ export default class CreateRoomPage extends Component {
         code: this.props.roomCode,
       }),
     };
-    fetch("/api/update-room", requestOptions)
-      .then((response) => {
-        if (response.ok){
-          this.setState({
-            successMsg: "Room updated Successfully"
-          })
-        } else {
-         this.setState({
-          errorMsg: "Error updating room..."
-         });
-        
-
-        }
-          this.props.updateCallback();
-      });
- 
+    fetch("/api/update-room", requestOptions).then((response) => {
+      if (response.ok) {
+        this.setState({
+          successMsg: "Room updated successfully!",
+        });
+      } else {
+        this.setState({
+          errorMsg: "Error updating room...",
+        });
+      }
+      this.props.updateCallback();
+    });
   }
 
-
-  renderCreateButton() {
-   return(
-    <Grid container spacing={1}>
-    <Grid item xs={12} align="center">
-    <Button
-      color="primary"
-      variant="contained"
-      onClick={this.handleRoomButtonPressed}
-    >
-      Create A Room
-    </Button>
-  </Grid>
-  <Grid item xs={12} align="center">
-    <Button color="secondary" variant="contained" to="/" component={Link}>
-      Back
-    </Button>
-  </Grid>
-  </Grid>
-   ) 
+  renderCreateButtons() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleRoomButtonPressed}
+          >
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
   }
 
-renderUpdateButtons() {
-  return (
-    <Grid item xs={12} align="center">
-    <Button
-      color="primary"
-      variant="contained"
-      onClick={this.handleUpdateButtonPressed}
-    >
-      Update Room
-    </Button>
-  </Grid>
-  )
-}
+  renderUpdateButtons() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={this.handleUpdateButtonPressed}
+        >
+          Update Room
+        </Button>
+      </Grid>
+    );
+  }
 
   render() {
     const title = this.props.update ? "Update Room" : "Create a Room";
 
-
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <Collapse in={this.state.errorMsg != "" || this.state.successMsg != "" }>
-            {this.state.successMsg != "" ? (<Alert severity="success" onClose={() => {this.setState({
-              successMsg: ""
-            });
-            }}>{this.state.successMsg}</Alert>): (<Alert severity="error" onClose={() => {this.setState({errorMsg: ""})
-            }} 
-            >{this.state.errorMsg}</Alert>)}
+          <Collapse
+            in={this.state.errorMsg != "" || this.state.successMsg != ""}
+          >
+            {this.state.successMsg != "" ? (
+              <Alert
+                severity="success"
+                onClose={() => {
+                  this.setState({ successMsg: "" });
+                }}
+              >
+                {this.state.successMsg}
+              </Alert>
+            ) : (
+              <Alert
+                severity="error"
+                onClose={() => {
+                  this.setState({ errorMsg: "" });
+                }}
+              >
+                {this.state.errorMsg}
+              </Alert>
+            )}
           </Collapse>
-          
-
         </Grid>
-
-
-
         <Grid item xs={12} align="center">
           <Typography component="h4" variant="h4">
             {title}
@@ -194,7 +198,9 @@ renderUpdateButtons() {
             </FormHelperText>
           </FormControl>
         </Grid>
-          {this.props.update ? this.renderUpdateButtons() : this.renderCreateButton()}
+        {this.props.update
+          ? this.renderUpdateButtons()
+          : this.renderCreateButtons()}
       </Grid>
     );
   }
